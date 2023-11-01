5577b32b4868c3c435f58f94e01ec34e8c5fddb4
임준수 <yimj57027@hotmail.com>
2023-11-01 20:32:17 +0900

    :sparkles: Feat: Project Copy

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css';
+import Router from './router/Router';
+import "./util/fcm.js"
+
+function App() {
+  return (
+    <>
+      <Router></Router>
+    </>
+  );
+}
+
+export default App;
---
+++ frontend/src/vite-env.d.ts
@@ -0,0 +1,7 @@
+/// <reference types="vite/client" />
+
+interface Window {
+    Kakao: any;
+}
+
+declare module 'react-read-pdf';
